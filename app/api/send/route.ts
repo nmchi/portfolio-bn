99d534d4f18deb7c743e7777c6ebe1a2e7589af7
BARON-PC\nmchi <baron.nguyen@finantaged.com>
2024-03-06 18:00:05 +0700

    route

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,18 +2,18 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req, res) {
-    const { email, subject, message } = req.body;
-    try {
-        const data = await resend.emails.send({
-            from: `${email}`,
-            to: ['nmchiht@gmail.com'],
-            subject: `${subject}`,
-            react: `${message}`,
-        });
+export async function POST() {
+    // const { email, subject, message } = req.body;
+    // try {
+    //     const data = await resend.emails.send({
+    //         from: `${email}`,
+    //         to: ['nmchiht@gmail.com'],
+    //         subject: `${subject}`,
+    //         react: `${message}`,
+    //     });
 
-        return res.status(200).json(data);
-    } catch (error) {
-        return res.status(400).json({ error });
-    }
+    //     return Res.status(200).json(data);
+    // } catch (error) {
+    //     return res.status(400).json({ error });
+    // }
 }
